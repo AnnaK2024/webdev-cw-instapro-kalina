7d76ce46e4d802615243a86cf0fb91f3eb50acdb
annak2024 <kalininaann.pk@mail.ru>
2025-04-23 23:38:19 +0300

    Появилась ошибка при получении постов

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,9 @@ export default defineConfig([
     },
     {
         files: ['**/*.{js,mjs,cjs}'],
-        languageOptions: { globals: globals.browser },
+        languageOptions: { globals: globals.browser }, env: {
+            browser: true,
+        }
     },
     config,
     plugin,
